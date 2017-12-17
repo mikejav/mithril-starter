8322eace5499398e396afa96fe5e0086918e10d6
mikejav <mikejav@users.noreply.github.com>
2017-12-17 16:30:47 +0100

    Move to jsx

---
+++ src/components/NavLink.js
@@ -0,0 +1,31 @@
+import m from "mithril"
+
+var NavLink = {
+  scrollToTop() {
+    console.log("scrolling to top")
+    window.scrollTo(0, 0)
+  },
+
+  oncreate(vnode) {
+    vnode.dom.addEventListener("click", this.scrollToTop)
+  },
+
+  view(vnode) {
+    let attrs = vnode.attrs
+
+    attrs.href = vnode.attrs.to
+    attrs.to = undefined
+
+    attrs.oncreate = m.route.link
+
+    attrs.className = "nav-link"
+
+    return (
+      <li className={"nav-item" + (m.route.get() == attrs.href ? " active" : "")}>
+        <a {...attrs}>{vnode.children}</a>
+      </li>
+    )
+  }
+}
+
+export default NavLink
